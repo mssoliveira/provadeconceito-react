5ddb57c32dac96296131a7511f048e7ad7b34bb9
Maurício Souza <mssoliveira1995@gmail.com>
2024-02-18 11:33:23 -0300

    counter in gamepad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,214 @@
-import Image from "next/image";
+'use client';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { randomUUID } from 'crypto';
+import { use, useEffect, useLayoutEffect, useState } from 'react';
+import { useGamepads } from 'react-gamepads';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [gamepads, setGamepads] = useState<any>({});
+	useGamepads((controller: any) => {
+		setGamepads(controller);
+	});
+	const [counterOne, setCounterOne] = useState<number>(0);
+	const [counterTwo, setCounterTwo] = useState<number>(0);
+	const [counterThree, setCounterThree] = useState<number>(0);
+	const [counterFour, setCounterFour] = useState<number>(0);
+	const [counterFive, setCounterFive] = useState<number>(0);
+	const [counterSix, setCounterSix] = useState<number>(0);
+	const [counterSeven, setCounterSeven] = useState<number>(0);
+	const [counterEight, setCounterEight] = useState<number>(0);
+	const [counterNine, setCounterNine] = useState<number>(0);
+	const [counterTen, setCounterTen] = useState<number>(0);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		if (gamepads && gamepads[0]) {
+			if (gamepads[0].buttons[0].pressed) {
+				setCounterOne(counterOne + 1);
+			}
+			if (gamepads[0].buttons[1].pressed) {
+				setCounterTwo(counterTwo + 1);
+			}
+			if (gamepads[0].buttons[2].pressed) {
+				setCounterThree(counterThree + 1);
+			}
+			if (gamepads[0].buttons[3].pressed) {
+				setCounterFour(counterFour + 1);
+			}
+			if (gamepads[0].buttons[4].pressed) {
+				setCounterFive(counterFive + 1);
+			}
+			if (gamepads[0].buttons[5].pressed) {
+				setCounterSix(counterSix + 1);
+			}
+			if (gamepads[0].buttons[6].pressed) {
+				setCounterSeven(counterSeven + 1);
+			}
+			if (gamepads[0].buttons[7].pressed) {
+				setCounterEight(counterEight + 1);
+			}
+			if (gamepads[0].buttons[8].pressed) {
+				setCounterNine(counterNine + 1);
+			}
+			if (gamepads[0].buttons[9].pressed) {
+				setCounterTen(counterTen + 1);
+			}
+		}
+	}, [gamepads && gamepads[0]]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const button0 =
+		gamepads && gamepads[0] && gamepads[0].buttons[0].pressed
+			? 'True'
+			: 'False';
+	const button1 =
+		gamepads && gamepads[0] && gamepads[0].buttons[1].pressed
+			? 'True'
+			: 'False';
+	const button2 =
+		gamepads && gamepads[0] && gamepads[0].buttons[2].pressed
+			? 'True'
+			: 'False';
+	const button3 =
+		gamepads && gamepads[0] && gamepads[0].buttons[3].pressed
+			? 'True'
+			: 'False';
+	const button4 =
+		gamepads && gamepads[0] && gamepads[0].buttons[4].pressed
+			? 'True'
+			: 'False';
+	const button5 =
+		gamepads && gamepads[0] && gamepads[0].buttons[5].pressed
+			? 'True'
+			: 'False';
+	const button6 =
+		gamepads && gamepads[0] && gamepads[0].buttons[6].pressed
+			? 'True'
+			: 'False';
+	const button7 =
+		gamepads && gamepads[0] && gamepads[0].buttons[7].pressed
+			? 'True'
+			: 'False';
+	const button8 =
+		gamepads && gamepads[0] && gamepads[0].buttons[8].pressed
+			? 'True'
+			: 'False';
+	const button9 =
+		gamepads && gamepads[0] && gamepads[0].buttons[9].pressed
+			? 'True'
+			: 'False';
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const gamePadCounter = [
+		{
+			gamepadButton: 'X ou A',
+			pressures: button0,
+			count: counterOne,
+		},
+		{
+			gamepadButton: 'Bola ou B',
+			pressures: button1,
+			count: counterTwo,
+		},
+		{
+			gamepadButton: 'Quadrado ou X',
+			pressures: button2,
+			count: counterThree,
+		},
+		{
+			gamepadButton: 'Triangulo ou Y',
+			pressures: button3,
+			count: counterFour,
+		},
+		{
+			gamepadButton: 'L1 ou LB',
+			pressures: button4,
+			count: counterFive,
+		},
+		{
+			gamepadButton: 'R1 ou RB',
+			pressures: button5,
+			count: counterSix,
+		},
+		{
+			gamepadButton: 'L2 ou LT',
+			pressures: button6,
+			count: counterSeven,
+		},
+		{
+			gamepadButton: 'R2 ou RT',
+			pressures: button7,
+			count: counterEight,
+		},
+		{
+			gamepadButton: 'SELECT',
+			pressures: button8,
+			count: counterNine,
+		},
+		{
+			gamepadButton: 'START',
+			pressures: button9,
+			count: counterTen,
+		},
+	];
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div>
+			<Card className="w-[800px]">
+				<CardHeader>
+					<CardTitle>
+						GamePad:{' '}
+						{gamepads && gamepads[0] && gamepads[0].connected
+							? 'True'
+							: 'False'}
+					</CardTitle>
+					<CardDescription>
+						{gamepads && gamepads[0] && gamepads[0].id
+							? gamepads[0].id
+							: ''}
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[300px]">
+									Mapeamento - PlayStation ou Xbox
+								</TableHead>
+								<TableHead>Pressionado</TableHead>
+								<TableHead>Contador ao Pressionar</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{gamePadCounter.map((gamePadCounter) => (
+								<TableRow key={gamePadCounter.gamepadButton}>
+									<TableCell className=" text-center">
+										{gamePadCounter.gamepadButton}
+									</TableCell>
+									<TableCell>
+										{gamePadCounter.pressures}
+									</TableCell>
+									<TableCell>
+										{gamePadCounter.count}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
